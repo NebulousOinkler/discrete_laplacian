162a2eaebbddd1d170f5a9caa9bf04b1701038ff
NebulousOinkler <sharatc.is.me@gmail.com>
2024-07-14 20:09:13 -0700

    fixed publish

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Activate the virtual environment
-source ../discrete_laplacian_env/bin/activate
+source ../discrete_laplacian_env/Scripts/activate
 
 # Generate the static files
 pelican content
